ed2ba9d5b9f85c6a096ff5a5a1adfeed22971c72
RWMumo <ritawanza5@gmail.com>
2022-01-07 10:45:00 +0300

    script placed correctly

--- js/dec-ip.js
+++ js/dec-ip.js
@@ -1,24 +1,31 @@
-const handleClick = () => {
+// formula
+const akanNameFormula =(CC, YY, MM, DD) => {
+console.log( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
+}
+function handleClick() {
     // user input
     const birthdayBox = document.getElementById("birthDay")
     const birthdayBox = document.getElementById("birthMonth")
     const birthdayBox = document.getElementById("birthYear")
     const birthdayBox = document.getElementById("birthGender")
-    
+
     // get value
-    const birthDay = birthdayBox.value;
-    const birthMonth = birthdayBox.value;
-    const birthYear = birthdayBox.value;
-    const birthGender = birthdayBox.value;
+    const birthDay = birthdayBox.value
+    const birthMonth = birthdayBox.value
+    const birthYear = birthdayBox.value
+    const birthGender = birthdayBox.value
 
     console.log(birthDay, birthMonth, birthYear, birthGender)
 
     // enter gender
-    const allGendersArray =["Male, Female"]
-    if(allGendersArray.includes(birthGender)) {
-        const centuryVar = String(birthYear).substring(0 ,2)
+    const allGendersArray = ["Male, Female"]
+    if (allGendersArray.includes(birthGender)) {
+        const centuryVar = String(birthYear).substring(0, 2)
         const yearVar = String(birthYear).substring(2)
-    } else{
+     akanNameFormula(centuryVar, yearVar, birthMonth, birthDay)
+
+
+    } else {
         alert("Please enter a valid answer")
     }
 
